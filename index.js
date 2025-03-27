a24bd87b1377d9b6c76dc68c4096f8ec29b3aa7a
dank1 <dankenrick34@yahoo.com>
2025-03-26 21:36:13 -0700

    finished toggling model

--- index.js
+++ index.js
@@ -3,6 +3,40 @@
 //publicKey: NQkhDt6mClnYfrttP
 
 function contact(event){
-    event.preventDefault()
-    console.log("it worked")
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+    
+    emailjs.sendForm(
+        'service_cqunt7c',
+        'template_0ha5z71',
+        event.target,
+        'NQkhDt6mClnYfrttP'
+    ).then(() =>{
+        //throw new Error("error");
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+        console.log("this worked")
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable  Please contact me directly on dankenrick34@yahoo.com"
+        );
+    })
+   setTimeout(() =>{
+
+    console.log('it worked 1')
+   }, 500);
+}
+
+let isModalOpen = false;
+function toggleModal(){
+    if(isModalOpen){
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true;
+    // toggle modal
+    document.body.classList += " modal--open";
 }
